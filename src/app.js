2bd943f7f06f94ccb17101ee8033705a9b7fa923
Marko <tomicmark3@gmail.com>
2023-02-22 20:14:36 +0100

    Changed some error message

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.get("/help", (req, res) => {
 
 app.get("/weather", (req, res) => {
   if (!req.query.address) {
-    return res.send({ error: "You must provide address" });
+    return res.send({ error: "You must provide address!" });
   }
 
   geocode(
